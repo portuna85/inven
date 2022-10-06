233d497ff9c35d68309d3274eda82c316f602789
portuna85 <portuna85@gmail.com>
2022-10-06 19:38:00 +0900

    개발환경 설정

--- src/test/java/com/inven/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/inven/book/springboot/web/HelloControllerTest.java
@@ -34,4 +34,18 @@ public class HelloControllerTest {
         // then
 
     }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 100000;
+
+        mvc.perform(get("/hello/dto")
+                   .param("name", name)
+                   .param("amount", String.valueOf(amount)))
+           .andExpect(status().isOk())
+           .andExpect(jsonPath("$.name", is(name)))
+           .andExpect(jsonPath("$.amount", is(amount)));
+
+    }
 }
